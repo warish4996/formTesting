04478a492e6aae54c5d366aeec7114627846171b
Warish4996 <warishce@gmail.com>
2020-09-07 11:46:54 +0530

    testing changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 
-function App() {
+export default ()=> {
+
+const [email, setEmail] = useState('');
+const [password, setPassword] = useState('');
+const [emailError, setEmailError] = useState(false);
+const [vaildEmailError, setValidEmailError] = useState(false);
+const [passwordError, setPasswordError] = useState(false);
+const [success, setSuccess] = useState('');
+
+const isEmail =  (email) => {
+  let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+const validateForm = () => {
+  if(!email) {
+    setEmailError(true);
+    return false;
+  }
+  else if(!isEmail(email)) {
+    setValidEmailError(true);
+    return false;
+  }else if(!password) {
+    setPasswordError(true);
+    return false;
+  }else{
+    return true;
+  }
+}
+
+
+const handleOnSubmit =(e)=>{
+  setEmailError(false);
+  setValidEmailError(false);
+  setPasswordError(false);
+
+  if(validateForm()){
+     setSuccess('Form Submit Done')
+  }
+}
+
   return (
+    <div className="main" data-test="component-app">
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Form</h1>
+     <h3><b>{success ? success :''}</b></h3>
+     <lable><b>Email</b></lable>
+     <input data-test="input1" value={email} onChange={(e)=>setEmail(e.target.value)} className='inputCss'/>
+     {emailError ? <p data-test="p1">please fill the Email</p>:''}
+     {vaildEmailError ? <p>Your Email is wrong</p>:''}
+     <label><b>Password</b></label>
+     <input data-test="input2" value={password} onChange={(e)=>setPassword(e.target.value)} className='inputCss'/>
+     {passwordError ? <p>please fill the Password</p>:''}
+     <button data-test="button" onClick={e =>handleOnSubmit(e)} className='buttonCss'>Submit</button>
+    </div>
     </div>
   );
 }
-
-export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,56 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { fireEvent } from '@testing-library/react';
 import App from './App';
+import { configure,shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+configure({adapter: new Adapter()})
+
+describe('Form',() =>{
+
+  let wrapper;
+
+  beforeEach(()=>{
+     wrapper = shallow(<App/>)
+  })
+
+  it('h1 tag',() =>{ 
+    const name = wrapper.find("h1").text()
+    expect(name).toContain('Form')
+  })
+
+  it('Should have one',() =>{
+    const component = wrapper.find("[data-test='component-app']");
+    expect(component.length).toBe(1);
+  })
+
+  it('button',() =>{
+    const name = wrapper.find("[data-test='button']").text()
+    expect(name).toBe('Submit')
+  })
+  it('On Click button',() =>{
+    const name = wrapper.find("[data-test='button']").simulate('click');
+    expect(wrapper.find("[data-test='p1']").text()).toBe("please fill the Email")
+  })
+
+  it('Should have input',() =>{
+    const input = wrapper.find("[data-test='input1']");
+    input.simulate('change', { target: { value: '' } });
+  })
+
+  it('both input check',()=>{
+    const input1 =wrapper.find("[data-test='input1']");
+    const input2 =wrapper.find("[data-test='input2']")
+    input1.simulate('change',{target: {value:'war@gmail.com'}})
+    input2.simulate('change',{target: {value:'war123'}})
+    const name = wrapper.find("[data-test='button']").simulate('click');
+    expect(wrapper.find("h3").text()).toBe("Form Submit Done")
+
+  })
+
+
+})
+
+
+
+
